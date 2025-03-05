337d06d691290dc3c6d9b6e819c5586e58399381
Iroha1132 <zhangyiyao422@gmail.com>
2025-03-05 19:22:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -75,5 +75,5 @@ app.post('/add-category', (req, res) => {
 
 // Start the server
 app.listen(3000, () => {
-    console.log('Server started on port 3000');
+    console.log('Server started on port');
 });
